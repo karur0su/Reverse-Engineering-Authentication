c30effaec5074078cc0786ece4ec5a28f0167ace
David White Wolf <mkarurosun@gmail.com>
2020-09-14 08:47:11 -0400

    Passport Pseudo code

--- Develop/config/passport.js
+++ Develop/config/passport.js
@@ -1,18 +1,20 @@
 
-// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
+// Passport to use Local Strategy. I.E, login with a username/email and password.
 
-// Our user will sign in using an email, rather than a "username"
+// User will sign in using an email, rather than a "username"
 
-// When a user tries to sign in this code runs
+// When a user tries to sign in this code runs.
 
-// If there's no user with the given email
+// If no user is detected with the given email.
   
-// If there is a user with the given email, but the password the user gives us is incorrect
+// If user with the given email, but the password the user gives us is incorrect.
    
-// If none of the above, return the user
+// If none of the above is populated, return the user.
 
-// In order to help keep authentication state across HTTP requests,
-// Sequelize needs to serialize and deserialize the user
-// Just consider this part boilerplate needed to make it all work
+// In order to keep authentication state across HTTP requests,
 
-// Exporting our configured passport
+// Sequelize needs to serialize and deserialize the user.
+
+// Just consider this part boilerplate needed to make it all work.
+
+// Exporting our configured passport.
