521e99adddce60b84a0d8e8d5662dece8b81fd83
David White Wolf <mkarurosun@gmail.com>
2020-09-14 08:17:53 -0400

    Psude code of Authenticated

--- Develop/config/middleware/isAuthenticated.js
+++ Develop/config/middleware/isAuthenticated.js
@@ -1,6 +1,7 @@
-// This is middleware for restricting routes a user is not allowed to visit if not logged in
+//Middleware for restricting routes a user not allowed to visit if user not logged in.
 
-  // If the user is logged in, continue with the request to the restricted route
+  
+// If user is logged in, continue with the request to the restricted route.
  
 
-  // If the user isn't logged in, redirect them to the login page
+  // If user NOT logged in, redirect to login page.
